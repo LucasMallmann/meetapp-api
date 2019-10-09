b5c69de9094280200e78599631af8901688584ed
Lucas Mallmann <lucas.ribeiro@climatempo.com.br>
2019-10-09 13:35:43 -0300

    feat: Added the Queue class

---
+++ src/lib/Queue.js
@@ -0,0 +1,46 @@
+import Bee from 'bee-queue';
+import redisConfig from '../config/redis';
+
+const jobs = [];
+
+class Queue {
+  constructor() {
+    this.queues = {};
+    this.init();
+  }
+
+  init() {
+    jobs.forEach(job => {
+      const { key, handle } = job;
+
+      this.queues[key] = {
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        handle,
+      };
+    });
+  }
+
+  /**
+   * Ads a new job to the queue
+   * @param {string} key Unique key of the job
+   * @param {any} job Data (parameters) that it's going to pass to the handle function
+   */
+  add(key, job) {
+    return this.queues[key].bee.createJob(job).save();
+  }
+
+  processQueue() {
+    jobs.forEach(job => {
+      const { handle } = this.queues[job.key];
+      job.on('failure', this.handleFailure).process(handle);
+    });
+  }
+
+  handleFailure(job, err) {
+    console.log(`Queue ${job.queue.name}: FAILED`, err);
+  }
+}
+
+export default new Queue();
